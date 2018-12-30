91e3be0b980b335c95d8c17504b1489eb8d90b0d
Joseph Yao <joseph.yao.ruozhou@gmail.com>
2018-12-30 16:24:39 +0800

    :sparkles: add unit test for birthday code

---
+++ birthday_unittest.cc
@@ -0,0 +1,20 @@
+#include "gtest/gtest.h"
+extern "C"
+{
+#include "birthday.h"
+#include "stub_today_provider.h"
+}
+
+namespace {
+
+    TEST(Birthday, NotABirthday) {
+        setToday(3, 15);
+        ASSERT_EQ(false, isBirthday());
+    }
+
+    TEST(Birthday, IsABirthday) {
+        setToday(4, 9);
+        ASSERT_EQ(true, isBirthday());
+    }
+
+}
