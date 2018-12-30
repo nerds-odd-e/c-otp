c10750bf1e64bc05d89c70d10274391fe8b51df0
Joseph Yao <joseph.yao.ruozhou@gmail.com>
2018-12-30 15:55:34 +0800

    :sparkles: add stub today provider code

---
+++ stub_today_provider.h
@@ -0,0 +1,18 @@
+#ifndef C_OTP_STUB_TODAY_PROVIDER_H
+#define C_OTP_STUB_TODAY_PROVIDER_H
+
+#include "time.h"
+
+struct tm today;
+
+void setToday(int month, int day) {
+    time_t now = time(0);
+    today = *localtime(&now);
+    today.tm_mon = month - 1;
+    today.tm_mday = day;
+}
+
+struct tm getToday() {
+    return today;
+}
+#endif //C_OTP_STUB_TODAY_PROVIDER_H
