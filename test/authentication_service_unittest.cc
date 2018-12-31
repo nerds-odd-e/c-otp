55e182b75b59ec0e2d3431edb77b24064499ae64
Joseph Yao <joseph.yao.ruozhou@gmail.com>
2018-12-31 16:47:22 +0800

    :sparkles: use fff as stub lib

--- test/authentication_service_unittest.cc
+++ test/authentication_service_unittest.cc
@@ -2,14 +2,22 @@
 extern "C"
 {
 #include "../main/authentication_service.h"
-#include "stub_rsa_token_dao.h"
-//#include "fff.h"
+#include "../main/rsa_token_dao.h"
+#include "fff.h"
 }
 
-//DEFINE_FFF_GLOBALS;
-//FAKE_VOID_FUNC(getRandom, char *, char *);
+DEFINE_FFF_GLOBALS;
+FAKE_VALUE_FUNC(int, getRandom, const char *, char *, size_t);
+
+const char* stubRandomCode;
+int getRandom_custom_fake(const char * userName, char * randomCode, size_t len) {
+    sprintf(randomCode, "%s", stubRandomCode);
+    return 0;
+}
 
 TEST(AuthenticationServiceTest, IsValid) {
+    getRandom_fake.custom_fake = getRandom_custom_fake;
+    stubRandomCode = "000000";
 
     ASSERT_TRUE(isValid("joey", "91000000"));
 }
